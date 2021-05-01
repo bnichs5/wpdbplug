5ae1bcfe48f87584d6da127b5c6cfa9be0b6d2ad
bnichs5 <bnichs51259@gmail.com>
2021-05-01 15:09:19 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'dbu691500' );
 define( 'DB_PASSWORD', 'Pirates1!!' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'db5002351170.hosting-data.io:3306' );
+define( 'DB_HOST', 'db5002351170.hosting-data.io' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );

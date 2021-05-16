506850dc9a71f1fbf0db362c5bd764c8d61ca45a
bnichs5 <bnichs51259@gmail.com>
2021-05-15 23:16:42 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', '' );
+define( 'DB_NAME', 'ZDkyBDFD0y' );
 
 /** MySQL database username */
-define( 'DB_USER', '' );
+define( 'DB_USER', 'ZDkyBDFD0y' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '2O4eIxrXZA' );
 
 /** MySQL hostname */
-define( 'DB_HOST', '' );
+define( 'DB_HOST', 'remotemysql.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );

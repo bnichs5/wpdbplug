c2707226632471ab56423dba5cfc13f6afa9c111
bnichs5 <bnichs51259@gmail.com>
2022-03-19 23:59:44 -0400

    Create temp4.php

---
+++ temp/temp4.php
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+function produce_XML_object_tree($raw_XML) {
+    libxml_use_internal_errors(true);
+    try {
+        $xmlTree = new SimpleXMLElement($raw_XML);
+    } catch (Exception $e) {
+        // Something went wrong.
+        $error_message = 'SimpleXMLElement threw an exception.';
+        foreach(libxml_get_errors() as $error_line) {
+            $error_message .= "\t" . $error_line->message;
+        }
+        trigger_error($error_message);
+        return false;
+    }
+    return $xmlTree;
+}
+
+$xml_feed_url = '******';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $xml_feed_url);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$xml = curl_exec($ch);
+curl_close($ch);
+
+$cont = produce_XML_object_tree($xml);
+
+
+
+
+
+
